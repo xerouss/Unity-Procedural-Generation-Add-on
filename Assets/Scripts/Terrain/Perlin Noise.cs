7d7c2dd7620df07b1bc96bd506bace5e3c2bff60
Xerouss <stephen.wong96@gmail.com>
2018-02-27 09:52:56 +0000

    Created the PN Grid Node. Set up the structure of the Perlin Noise Script.

---
+++ Assets/Scripts/Terrain/Perlin Noise.cs
@@ -0,0 +1,36 @@
+// ***********************************************************************************
+//	Name:	           Stephen Wong
+//	Last Edited On:	   27/02/2018
+//	File:			   Perlin Noise.cs
+//	Project:		   Procedural Generation Add-on
+// ***********************************************************************************
+
+////////////////////////////////////////////
+// Libraries and namespaces
+using UnityEngine;
+
+////////////////////////////////////////////
+
+/// <summary>
+/// This class will be used to create levels using Perlin Noise
+/// </summary>
+namespace ProceduralGenerationAddOn
+{
+    public class PerlinNoise : MonoBehaviour
+    {
+        PNGridNode[] m_grid;
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
