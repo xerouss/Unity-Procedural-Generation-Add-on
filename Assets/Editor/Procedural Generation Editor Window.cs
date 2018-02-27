c9222ef739a866e5eb93f5676de45c41de4813e0
Xerouss <stephen.wong96@gmail.com>
2018-02-27 10:43:22 +0000

    Created the editor window script in order to test the Perlin Noise. Got the Perlin Noise script to create the grid to use.

---
+++ Assets/Editor/Procedural Generation Editor Window.cs
@@ -0,0 +1,62 @@
+// ***********************************************************************************
+//	Name:	           Stephen Wong
+//	Last Edited On:	   27/02/2018
+//	File:			   Procedural Generation Editor Window.cs
+//	Project:		   Procedural Generation Add-on
+// ***********************************************************************************
+
+////////////////////////////////////////////
+// Libraries and namespaces
+using UnityEngine;
+using UnityEditor;
+
+////////////////////////////////////////////
+
+/// <summary>
+/// This is responsible for the window which holds everything in the add-on
+/// </summary>
+namespace ProceduralGenerationAddOn
+{
+    public class ProceduralGenerationEditorWindow : EditorWindow
+    {
+
+        #region Variables
+
+        #region Private
+
+        PerlinNoise m_perlinNoise;
+
+        #endregion
+
+        #endregion
+
+        // Where to access the window
+        [MenuItem("Window/Procedural Generate Level")]
+
+        // On start of the program
+        private void Awake()
+        {
+            m_perlinNoise = new PerlinNoise();
+        }
+
+        /// <summary>
+        /// How to display the window
+        /// </summary>
+        public static void ShowWindow()
+        {
+            EditorWindow.GetWindow(typeof(ProceduralGenerationEditorWindow));
+        }
+
+        /// <summary>
+        /// What to display in the window
+        /// </summary>
+        void OnGUI()
+        {
+            // Temp, here to test the level creation
+            if(GUI.Button(new Rect(10, 10, 150, 50), "Generate Level"))
+            {
+                m_perlinNoise.CreateGrid(new Vector2(10, 10));
+            }
+        }
+    }
+}
--- Assets/Scripts/Terrain/Perlin Noise.cs
+++ Assets/Scripts/Terrain/Perlin Noise.cs
@@ -16,21 +16,39 @@ using UnityEngine;
 /// </summary>
 namespace ProceduralGenerationAddOn
 {
-    public class PerlinNoise : MonoBehaviour
+    public class PerlinNoise
     {
-        PNGridNode[] m_grid;
+        // TODO: Add more customisation for the user
+        PNGridNode[,] m_grid;
 
-
-        // Use this for initialization
-        void Start()
+        // TODO: Add extra parameters for the user to change
+        public void CreateGrid(Vector2 size)
         {
+            // Create the grid with the passed size
+            m_grid = new PNGridNode[(int)size.x, (int)size.y];
 
+            Vector3 gradient;
+
+            // Fill the grid with random gradients
+            for (int x = 0; x < size.x; x++)
+            {
+                for (int y = 0; y < size.y; y++)
+                {
+                    // TODO: Change the range of these to the user's inputted ones
+                    gradient = new Vector3(Random.Range(0, 10), 
+                        Random.Range(0, 10), 
+                        Random.Range(0, 10));
+
+                    m_grid[x, y].Gradient = gradient;
+                }
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        public bool Interpolate()
         {
 
+
+            return true;
         }
     }
 }
